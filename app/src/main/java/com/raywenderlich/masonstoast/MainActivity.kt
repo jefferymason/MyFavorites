2ff47e1e46ddd3ab78120fd2a185eea13b05b4fc
Jeffery Wayne Mason <jefferymason72@gmail.com>
2020-09-07 12:48:58 -0500

    Completed Project

--- app/src/main/java/com/raywenderlich/masonstoast/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/masonstoast/MainActivity.kt
@@ -21,4 +21,49 @@ class MainActivity : AppCompatActivity() {
     fun displayToastMsg(v: View?) {
         toastMsg("My favorite color is red.")
     }
+
+    private fun toastMsg2(msg: String?) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    fun displayToastMsg2(v: View?) {
+        toastMsg("My favorite food is pizza.")
+    }
+
+    private fun toastMsg3(msg: String?) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    fun displayToastMsg3(v: View?) {
+        toastMsg("My favorite tv show is Frasier.")
+    }
+
+    private fun toastMsg4(msg: String?) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    fun displayToastMsg4(v: View?) {
+        toastMsg("My favorite artist is Prince.")
+    }
+
+    private fun toastMsg5(msg: String?) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    fun displayToastMsg5(v: View?) {
+        toastMsg("My favorite day of the week is Wednesday.")
+    }
+
+    private fun toastMsg6(msg: String?) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    fun displayToastMsg6(v: View?) {
+        toastMsg("My favorite season is summer.")
+    }
 }
